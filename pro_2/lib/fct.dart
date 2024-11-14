79656d3b8c2b4d1a8cafe0192e5501a9a8be2902
abde-salek <abdelghanisalek@gmail.com>
2024-11-14 08:15:10 +0100

    StatefulBuilder

--- pro_2/lib/fct.dart
+++ pro_2/lib/fct.dart
@@ -9,18 +9,63 @@ class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _TaskListState createState() => _TaskListState();
 }
 
 class _TaskListState extends State<TaskList> {
-  
+
+  String taskName= "";
+  bool completed = false;
+
   //method of showdialog
-  void showDialog(BuildContext context) {
-    showDialog(context, builder: ( BuildContext context){
-      return const AlertDialog(
-        required.TaskModel.title,
-        required.TaskModel.completed
-      );
+  void showTask() {
+    bool tempChecked = completed; // Temporary variable for dialog
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setDialogState){
+          return const AlertDialog(
+            title: Text("EDIT"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                    // onChanged: (value) {
+                    //   setDialogState(() {
+                    //     taskName = value;
+                    //   },);
+                    // },
+                  decoration: InputDecoration(
+                    labelText: "Task Title",
+                    hintText: "Enter your task",
+                  ),
+                  value: taskName,
+                ),
+                Checkbox(
+                  value: completed, onChanged: (value) {
+                    setState(() {
+                      completed = value;
+                    });
+                  },)
+                
+              ],
+            ),
+        );
+        });        
+        
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: completed , onChanged: onChanged)
+      ],
+    );
   }
 }
