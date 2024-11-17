fcf0f08c58abb88281fb8f34911397a155be98a8
abde-salek <abdelghanisalek@gmail.com>
2024-11-17 08:40:57 +0100

    condition 1

--- pro_2/lib/fct.dart
+++ pro_2/lib/fct.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
-class TaskModel{
-  final String title= 'task';
-  final bool completed = false || true ;
-}
-
 class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _TaskListState createState() => _TaskListState();
+  TaskListState createState() => TaskListState();
 }
 
-class _TaskListState extends State<TaskList> {
+class TaskListState extends State<TaskList> {
 
-  String taskName= "";
+  static String taskName= "";
   bool completed = false;
-
+    List<Map<String, dynamic>> tasks = [];
+    List<String> tasksV = [];
   //method of showdialog
   void showTask() {
     bool tempChecked = completed; // Temporary variable for dialog
@@ -27,24 +23,19 @@ class _TaskListState extends State<TaskList> {
       builder: (BuildContext context) {
         return StatefulBuilder(
           builder: (context, setDialogState){
-          return const AlertDialog(
-            title: Text("EDIT"),
+          return AlertDialog(
+            title: const Text("EDIT"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
-                    // onChanged: (value) {
-                    //   setDialogState(() {
-                    //     taskName = value;
-                    //   },);
-                    // },
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: "Task Title",
                     hintText: "Enter your task",
                   ),
-                  value: taskName,
+                  value: TaskListState.taskName,
                 ),
-                Checkbox(
+                const Checkbox(
                   value: completed, onChanged: (value) {
                     setState(() {
                       completed = value;
