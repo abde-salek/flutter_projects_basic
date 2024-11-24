c915da3265cb9323f8207bf157397e39cfe1c7fe
abde-salek <abdelghanisalek@gmail.com>
2024-11-24 12:01:30 +0100

    ShowDialog DONE.

--- pro_2/lib/fct.dart
+++ pro_2/lib/fct.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
@@ -11,7 +12,7 @@ class TaskList extends StatefulWidget {
 class TaskListState extends State<TaskList> {
 
   List<String> tasks = [ ]; // List of tasks
-  static String taskName= "";
+  String taskName= "TF is wrong";
   bool completed = false;
   
   //method of showdialog
@@ -19,15 +20,15 @@ class TaskListState extends State<TaskList> {
     //bool tempChecked = completed; // Temporary variable for dialog
     showDialog(
       context: context,
-      builder: (BuildContext context) {
+      builder: (context) {
         return StatefulBuilder(
           builder: (context, setDialogState){
           return AlertDialog(
-            title: const Text("EDIT"),
+            title: const Text("CRUCH IT"),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const SizedBox(height: 8,),
+                const SizedBox(height: 7,),
                 TextField(
                   decoration: const InputDecoration(
                     labelText: "Task Title",
@@ -40,11 +41,12 @@ class TaskListState extends State<TaskList> {
                   },
                   
                 ),
+                const SizedBox(height: 8, width: 8,),
               ],
             ),
-          actions: [
+            actions: [
               Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   TextButton(
                     onPressed: () {
@@ -56,23 +58,83 @@ class TaskListState extends State<TaskList> {
                           Navigator.pop(context); // Close the dialog
                         }
                       },
-                      child: const Text("Save"),
+                    child: const Text("Save", style: TextStyle(color: Colors.green),),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Close', style: TextStyle(color: Colors.red),
+                    ),
                   ),
                 ],
               ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('Close'),
-              ),
-          ],
+            ],
         );
         });        
         
-    });
+      }
+    );
   }
-
+  
+   // Create an instance
+   // Call using the instance
+  bool showCenterWidget = true;
+  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return Container(); // Placeholder for demo
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            "TO DO LIST",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.amber[600],
+          elevation: 10,
+        ),
+      body: showCenterWidget
+              ?  const Center(child: Text("No Tasks for Today"),)
+              :  ListView.builder(
+          //itemCount: TaskListState.taskName.length,
+          itemBuilder: (BuildContext context, int index) {  
+            index += 1;
+          Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(12.0),
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[500],
+                    borderRadius: const BorderRadius.all(Radius.circular(7)),
+                  ),
+                    child: const Text(
+                      "TF is wrong",
+                    style: TextStyle(
+                      fontWeight:FontWeight.bold,
+                      fontSize: 24, // Font size
+                ),
+                    ),
+                ),),
+            ],
+          );
+          return null;},
+        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow[500],
+        onPressed: () {
+          showTask();
+            if (showCenterWidget) { 
+            showCenterWidget = false; // Switch value
+            }},
+          child: const Icon(
+            Icons.add,
+            ),
+    ),
+    ) ; // Placeholder for demo
   }
 }
--- pro_2/lib/main.dart
+++ pro_2/lib/main.dart
@@ -20,72 +20,13 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   bool showCenterWidget = true;
   
-  void switching(){
-    setState(() {
-      showCenterWidget =false;
-    });
-  }
-  
-  var UnObject = TaskListState(); // Create an instance
-   // Call using the instance
-  
   //ListView.builder();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home:  Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "TO DO LIST",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.amber[600],
-          elevation: 10,
-        ),
-        body: showCenterWidget
-
-              ?  Center(child: Text("No Tasks for Today"),)
-              :  ListView.builder (
-          itemCount: TaskListState.taskName.length,
-          itemBuilder: (BuildContext context, int index) {  
-            index += 1;
-          child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(12.0),
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.yellow[500],
-                    borderRadius: BorderRadius.all(Radius.circular(7)),
-                  ),
-                    child: Text(
-                      TaskListState.taskName,
-                    style: TextStyle(
-                      fontWeight:FontWeight.bold,
-                      fontSize: 24, // Font size
-                ),
-                    ),
-                ),),
-            ],
-          );
-          return null;},
-        ),
-        
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.yellow[500],
-        onPressed: () {switching();
-                      UnObject.showTask();},     //calling using an instance
-          child: Icon(
-            Icons.add,
-            ),
-    ),),);
+      home:  TaskList(),
+      
+      );
   }
 }
