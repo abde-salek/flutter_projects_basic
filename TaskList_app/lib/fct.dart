cfbd0dc6e3b14cb9ec8c026a7ba29597909ac6d6
abde-salek <abdelghanisalek@gmail.com>
2024-12-19 18:48:33 +0100

    list is not working well and final exams are so close

--- TaskList_app/lib/fct.dart
+++ TaskList_app/lib/fct.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_local_variable
 
+
 import 'package:flutter/material.dart';
 
 
@@ -12,14 +13,27 @@ class TaskList extends StatefulWidget {
 }
 
 class TaskListState extends State<TaskList> {
-
    // List of tasks
   late String? taskName;
   bool completed = false;
-  List<String> tasks = [];
-  
+  List<String> tasks= [];
+  int len=0, i=0;
+  int count = 0;
+  String tempTask= "";
+  // TextEditingController taskController = TextEditingController();
+
+  void addTask() {
+    tasks.add(taskName!);
+    }
+  String taskPrint(){
+    for (int i = 0; i <= taskName!.length; i++){
+      tempTask= tempTask + tasks[i];
+    };
+    i= i + taskName!.length;
+  return tempTask;
+  }
   //method of showdialog
-  void showTask() {
+  void showTask(){
     //bool tempChecked = completed; // Temporary variable for dialog
     showDialog(
       context: context,
@@ -38,9 +52,10 @@ class TaskListState extends State<TaskList> {
                     hintText: "Enter your task",
                   ),
                   onChanged: (value) {
-                    taskName = value;
-                    if (taskName != null)
-                    {tasks.add(taskName !);}
+                    taskName = value; 
+                    addTask();
+                    // if (taskName != null)
+                    //     {}
                   },
                 ),
                 const SizedBox(height: 8, width: 8,),
@@ -55,7 +70,8 @@ class TaskListState extends State<TaskList> {
                         // Save the task when 'Save' is pressed
                         if (taskName?.isNotEmpty ?? false) { // Trim to avoid spaces-only tasks
                           setState(() {
-                            tasks.add(taskName!); // Add the trimmed task
+                            tasks.add(taskName!);
+                            count += 1;
                           });
                            // Reset taskName
                           Navigator.of(context).pop(); // Close the dialog
@@ -83,7 +99,7 @@ class TaskListState extends State<TaskList> {
    // Create an instance
    // Call using the instance
   bool showCenterWidget = true;
-  int index= 0;
+
   // This widget is the root of your application.
 
   @override
@@ -104,8 +120,8 @@ class TaskListState extends State<TaskList> {
       body: showCenterWidget
               ?  const Center(child: Text("No Tasks for Today"),)
               :  ListView.builder(
-          itemCount: index,
-          itemBuilder: (BuildContext context, int index ) {  
+          itemCount: count,
+          itemBuilder: (context, count) {  
           return Row(
           mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -117,23 +133,14 @@ class TaskListState extends State<TaskList> {
                     color: Colors.yellow[500],
                     borderRadius: const BorderRadius.all(Radius.circular(7)),
                   ),
-                    child: Text(
-                    tasks[index], // Task Name
-                    style: const TextStyle(
-                      fontWeight:FontWeight.bold,
-                      fontSize: 24, // Font size
-                    ),
-                    ),
+                  child: Text(taskPrint()),
                 ),),
-            ],
-          ); 
-          }
-        
-        ),
+            ]
+          );}
+          ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.yellow[500],
         onPressed: () {
-          index = index + 1;
           showTask();
             if (showCenterWidget) { 
             showCenterWidget = false; // Switch value
